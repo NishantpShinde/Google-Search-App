a69a78eb7f3ca30ed507a56c186cd71a8c9240d1
Nishant Shinde <imnishant333@gmail.com>
2024-07-06 13:07:32 +0530

    Second commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,14 +7,12 @@ const BASE_URL = "https://www.googleapis.com/customsearch/v1";
 // copy api_key and cx key from the url and paste it below in params}
 
 // const params = {
-//     key:"AIzaSyC9uLPEVx6txAZvD9nr4KCRLUvQOWW7vNo",
-//     cx:"056d399e67b194a24"
+//    
 // }
 
 
 const params = {
-    key:"AIzaSyAQ7qrMfyOWqN2jOQibUemqCnh-hd39GWE",
-    cx:"d3e070fdf1ed842ef"
+    
 }
 
 export const fetchDataFromApi = async (payload) => {
